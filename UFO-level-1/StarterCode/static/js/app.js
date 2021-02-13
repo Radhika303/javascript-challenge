79246d1099ca8383175e4bedc45c846bfa3f29bc
Radhika303 <radhikasagar@Radhikas-MacBook-Pro.local>
2021-02-13 18:36:15 -0500

    test

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 // select tbody 
 tbody = d3.select("tbody")
 
-// append objects from data.js into new table rows inside table body
+// append objects into a new row 
 tableData.forEach(item => {
     var tr = tbody.append('tr');
     tr.append('td').text(item.datetime);
@@ -15,29 +15,29 @@ tableData.forEach(item => {
     tr.append('td').text(item.comments);
 });
 
-// create button variable to be used in function
+// Create button
 var button = d3.select('#filter-btn');
 
-// function that will get user input from form and use it to filter upon clicking button
+// Input from button 
 button.on('click', function() {
     // make variables for inputs and values for date and city
     var dateInput = d3.select('#date-filter');
     var dateValue = dateInput.property('value');
 
- // use input to filter data by date
+ // Filter data by date
  var filtered = tableData.filter(item => item.datetime === dateValue)
 
-  // clear table info before appending filtered data
+  // Trying to clear filter 
   tbody.html(``);
-  // adding filtered data
+  // Lastly adding the data. 
   filtered.forEach( item => {
       var tr = tbody.append('tr');
       tr.append('td').text(item.datetime);
       tr.append('td').text(item.city);
       tr.append('td').text(item.state);
-      tr.append('td').text(item.country);
       tr.append('td').text(item.shape);
       tr.append('td').text(item.durationMinutes);
       tr.append('td').text(item.comments);
+      tr.append('td').text(item.country);
   });
 });
